d4e4acd5c74e064d40d1965704be5ec7d3565d16
Zakkios <laskari.zakaria@gmail.com>
2023-08-17 11:09:47 +0200

    feature add : display posts by userId

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
+import Home from './Home'
 
 const About = () => {
   interface Post {
@@ -9,22 +10,45 @@ const About = () => {
     "body": string
   }
   const { pathname } = useLocation()
+  const currentUserId:number = parseInt(pathname.split('/')[2])
   const [ posts, setPosts ] = useState<Post[]>([])
+
+  useEffect(() => {
+    if(!isNaN(currentUserId)) {
+      getPosts()
+      console.log(true)
+    } else {
+      console.log(false)
+    }
+  }, [currentUserId])
+
+  const displayPosts = () => {
+    if(isNaN(currentUserId)) {
+      return <Home />
+    } else {
+      if(posts.length === 0) {
+        return 'chargement'
+      } else {
+        return posts.filter((post:Post) => post.userId === currentUserId).map((post:Post) => { 
+          return (<ul key={post.id} className='my-3 text-center'>
+            <li><span className='font-bold'>Titre : </span><br />{post.title}</li>
+            <li><span className='font-bold'>Contenu : </span><br />{post.body}</li>
+            </ul>)})
+      }
+    }
+  }
+
   const getPosts = () => {
     fetch("https://jsonplaceholder.typicode.com/posts").then(data => data.json()).then(response => setPosts(response))
   }
 
-  useEffect(() => {
-    getPosts()
-  }, [])
 
   return (
     <div className='max-w-screen-xl mx-auto'>
-      { posts.length === 0 ? "chargement" : posts.filter((post:Post) => post.userId === 10).map((post:Post) => { 
-        return (<ul key={post.id} className='my-3 text-center'>
-          <li><span className='font-bold'>Titre : </span><br />{post.title}</li>
-          <li><span className='font-bold'>Contenu : </span><br />{post.body}</li>
-          </ul>)}) }
+      <h1 className='text-3xl font-bold text-center my-9'>
+        { isNaN(currentUserId) ? 'Selectionnez un utilisateur' : `Articles de l'utilisateur ${ currentUserId }` }
+      </h1>
+      { displayPosts() }
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   interface User {
@@ -30,7 +31,13 @@ const Home = () => {
     <>
       <h1 className="text-3xl font-bold text-center my-5">Liste des utilisateurs : </h1>
       <ul className='text-center'>
-        { users.length === 0 ? "chargement" : users.map((user:User) => <li key={user.id}>{user.name}</li>) }
+        { users.length === 0 ? "chargement" : users.map((user:User) => { 
+          return (
+            <li key={user.id} className='my-5'>
+                <Link to={`/about/${user.id}`} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded'> 
+                {user.name} 
+                </Link>
+            </li>)}) }
       </ul>
     </>
   );
